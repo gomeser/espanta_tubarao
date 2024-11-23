eec8a655306ab100e31040714bc9c6a563424419
gomeser <163963522+gomeser@users.noreply.github.com>
2024-11-23 18:47:52 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,164 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PROJETO ESPANTA TUBARÃO</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+</head>
+<body>
+    <div id="particles-js"></div>
+    <section class="home">
+        <img src="images/ace.png" alt="image">
+
+        <div class="login-screen">
+            <div class="title">
+                <h1>Bem vindo(a), colaborador(a)!</h1>
+                <h2>Faça seu login e elimine dúvidas.</h2>
+            </div>
+
+            <form action="">
+                <div class="input">
+                    <label for="">Código do colaborador</label>
+                    <input type="text" placeholder="Seu código de colaborador">
+                </div>
+                <div class="input">
+                    <label for="">Email</label>
+                    <input type="text" placeholder="Seu melhor email">
+                </div>
+                <div class="input">
+                    <label for="">Senha</label>
+                    <input type="text" placeholder="Sua senha indicada">
+                </div>
+
+                <div class="checkbox">
+                    <input type="checkbox">
+                    <label for="">Concordo com o tratamento dos meus dados para finalidade de avaliação, direcionamento e planejamento tático e estratégico para a empresa.</label>
+                </div>
+
+                <button type="submit">
+                    Fazer login
+                </button>
+            </form>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+    <script>
+        particlesJS("particles-js", {
+            "particles": {
+                "number": {
+                    "value": 200,
+                    "density": {
+                        "enable": true,
+                        "value_area": 800
+                    }
+                },
+                "color": {
+                    "value": "#FFFFFF"  /* Cor das partículas alterada para branco */
+                },
+                "shape": {
+                    "type": "circle",
+                    "stroke": {
+                        "width": 0,
+                        "color": "#000000"
+                    },
+                    "polygon": {
+                        "nb_sides": 5
+                    }
+                },
+                "opacity": {
+                    "value": 0.5,
+                    "random": false,
+                    "anim": {
+                        "enable": false,
+                        "speed": 1,
+                        "opacity_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "size": {
+                    "value": 3,
+                    "random": true,
+                    "anim": {
+                        "enable": false,
+                        "speed": 40,
+                        "size_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "line_linked": {
+                    "enable": true,
+                    "distance": 150,
+                    "color": "#FFFFFF",  /* Alteração para branco */
+                    "opacity": 0.4,
+                    "width": 1
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 6,
+                    "direction": "none",
+                    "random": false,
+                    "straight": false,
+                    "out_mode": "out",
+                    "bounce": false,
+                    "attract": {
+                        "enable": false,
+                        "rotateX": 600,
+                        "rotateY": 1200
+                    }
+                }
+            },
+            "interactivity": {
+                "detect_on": "canvas",
+                "events": {
+                    "onhover": {
+                        "enable": true,
+                        "mode": "repulse"
+                    },
+                    "onclick": {
+                        "enable": true,
+                        "mode": "push"
+                    },
+                    "resize": true
+                },
+                "modes": {
+                    "grab": {
+                        "distance": 400,
+                        "line_linked": {
+                            "opacity": 1
+                        }
+                    },
+                    "bubble": {
+                        "distance": 400,
+                        "size": 40,
+                        "duration": 2,
+                        "opacity": 8,
+                        "speed": 3
+                    },
+                    "repulse": {
+                        "distance": 200,
+                        "duration": 0.4
+                    },
+                    "push": {
+                        "particles_nb": 4
+                    },
+                    "remove": {
+                        "particles_nb": 2
+                    }
+                }
+            },
+            "retina_detect": true
+        });
+    </script>
+</body>
+
+</html>
